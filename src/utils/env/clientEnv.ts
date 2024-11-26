7c229dbb1f12eec69498d78210e66da8146b0b91
Mohammad Hosein Lashani <sapumr@gmail.com>
2024-11-26 12:29:15 +0330

    add client and server env

---
+++ src/utils/env/clientEnv.ts
@@ -0,0 +1,6 @@
+import { createEnv } from "@t3-oss/env-nextjs";
+
+export const clientEnv = createEnv({
+  client: {},
+  runtimeEnv: {},
+});
---
+++ src/utils/env/serverEnv.ts
@@ -0,0 +1,10 @@
+import { createEnv } from "@t3-oss/env-nextjs";
+import { z } from "zod";
+
+export const serverEnv = createEnv({
+  server: {
+    DISCORD_BOT_TOKEN: z.string().min(1),
+    DISCORD_ALLOWED_CHANNELS: z.string().min(1),
+  },
+  experimental__runtimeEnv: process.env,
+});
