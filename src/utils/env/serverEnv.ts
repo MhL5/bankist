06c5cbbfb8ca03b8ba41de2a57eb392f5f7d1b8f
Mohammad Hosein Lashani <sapumr@gmail.com>
2024-11-26 13:05:26 +0330

    update server env

--- src/utils/env/serverEnv.ts
+++ src/utils/env/serverEnv.ts
@@ -5,6 +5,7 @@ export const serverEnv = createEnv({
   server: {
     DISCORD_BOT_TOKEN: z.string().min(1),
     DISCORD_ALLOWED_CHANNELS: z.string().min(1),
+    DISCORD_BOT_ERROR_LOGS_CHANNEL_ID: z.string().min(1),
   },
   experimental__runtimeEnv: process.env,
 });
